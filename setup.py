37c2d969babacc2a89dcdfd9032db767fd3cec0e
Hemangi Mistari <hemangimistari187@gmail.com>
2026-08-05 23:58:40 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+name = 'Generative AI Project',
+version= '0.0.0',
+author= 'Hemangi Mistari',
+author_email= 'hemangimistari187@gmail.com',
+packages= find_packages(),
+install_requires = []
+)
